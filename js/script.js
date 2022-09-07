d585bd6d06c9904d8f2111c51cb33c31845d7677
Venturaflrs99 <venturaflrs99@gmail.com>
2022-09-06 21:50:51 -0600

    function to remove open-class using JQuery

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const btnMenu = document.querySelector(".btn-mobile");
 btnMenu.addEventListener('click',function(){
     nav.classList.toggle("open");
 })
+
+$(window).resize(function() {
+    var $windowWidth = $(window).width();
+    if($windowWidth>820) {
+        if($('#nav-main').hasClass("open")){
+            $('#nav-main').removeClass("open");
+        }
+    }
+  }); 
